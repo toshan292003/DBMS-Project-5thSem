7fe1041c95c3e53bcb75bd6736705b4b5d449f2d
Toshan <toshanyt@gmail.com>
2024-03-01 20:39:23 +0530

    carousel complete

--- myapp/src/Components/Carousel.jsx
+++ myapp/src/Components/Carousel.jsx
@@ -1,41 +1,44 @@
 import React from "react";
-import './carousel.css';
-import Slider from 'react-slick';
+import "./carousel.css";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-export default function Carousel(){
+export default function Carousel() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    cssEase: "ease-in-out",
+  };
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+  const details = [
+    {
+      img: "https://images.pexels.com/photos/164634/pexels-photo-164634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/70912/pexels-photo-70912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/248747/pexels-photo-248747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
 
-    const details = [
-        {
-            img : "https://images.pexels.com/photos/164634/pexels-photo-164634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        },
-        {
-            img : "https://images.pexels.com/photos/70912/pexels-photo-70912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        },
-        {
-            img : "https://images.pexels.com/photos/248747/pexels-photo-248747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        }
-    ]
-
-    return(
-        <>
-            <div className="carou">
-                <Slider {...settings}>
-                    {details.map((d)=>(
-                        <div>
-                            <img src={d.img}/>
-                        </div>
-                    ))}
-                </Slider>
+  return (
+    <>
+      <div className="carou">
+        <Slider {...settings}>
+          {details.map((d) => (
+            <div>
+              <img src={d.img} />
             </div>
-        </>
-    )
+          ))}
+        </Slider>
+        <section>
+            
+        </section>
+      </div>
+    </>
+  );
 }
