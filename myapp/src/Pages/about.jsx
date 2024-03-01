c165129bc514a30c40130d22b95b58f1b1d60788
toshan292003 <toshanyt@gmail.com>
2024-03-01 16:40:07 +0530

    fsdgdg

--- myapp/src/Pages/about.jsx
+++ myapp/src/Pages/about.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import './about.css';
 
-export default function About(){
-    return(
+export default function About() {
+    return (
         <>
-            <section className="about-1">
-                <h1>about us</h1>
-            </section>
+            <div className="homedec"></div>
+            <div className="homebox">
+                <div>
+                    <h1 id="about-1">ABOUT US</h1>
+                </div>
+                <span className="home-items home-tri"></span>
+                <span className="home-items home-circle"></span>
+                <span className="home-items home-triangle"></span>
+                <span className="home-items home-tri"></span>
+            </div>
             <section className="about-2">
                 <h1></h1>
             </section>
