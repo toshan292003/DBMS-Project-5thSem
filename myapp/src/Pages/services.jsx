bf404e7bc1a13439c8321d02c8acd8ab43baa5bd
toshan292003 <toshanyt@gmail.com>
2024-03-18 00:31:24 +0530

    sds

--- myapp/src/Pages/services.jsx
+++ myapp/src/Pages/services.jsx
@@ -22,17 +22,29 @@ export default function Services() {
 
   const details = [
     {
+      img:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Kasavanahalli_lake.jpg/2560px-Kasavanahalli_lake.jpg",
+      name:"Kasavanahalli Lake"
+    },
+    {
       img: "https://pbs.twimg.com/media/Etg_8rxUcAoMxH1.jpg",
       name: "Netravathi"
     },
     {
-      img: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Cauvery_Kaveri_River_Karnataka_India_%282%29.jpg",
-      name: "Netravathi"
+      img: "https://images.indianexpress.com/2023/05/2-97.jpg",
+      name: "Basvanapura Lake"
     },
     {
-      img: "https://live.staticflickr.com/4511/37447307270_0438d76443_b.jpg",
-      name: "Netravathi"
+      img: "https://lh5.googleusercontent.com/p/AF1QipPKfYo5pwsbZwElLEQ6g1uEPivSRIAVlmWqKhfo=w480-h300-k-n-rw",
+      name: "Nelamangala Lake"
     },
+    {
+      img: "https://content.jdmagicbox.com/comp/bangalore/l4/080pxx80.xx80.220128232922.y9l4/catalogue/sarakki-lake-jp-nagar-7th-phase-bangalore-tourist-attraction-1yo9vnvw7f.jpg",
+      name: "Sarakki Lake"
+    },
+    {
+      img: "https://content.jdmagicbox.com/comp/bangalore/d6/080pxx80.xx80.220328233113.q9d6/catalogue/muthanallur-lake-muthanallur-bangalore-tourist-attraction-hj1dbcatev.jpg",
+      name: "Rayasandra Lake"
+    }
   ];
 
   return (
